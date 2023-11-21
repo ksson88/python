208ea9ea0e51df95cb1dc31cdd91b9df982e90ed
ksson88 <sks88@onycom.com>
2023-11-21 16:52:44 +0900

    2-7 불(Bool) 자료형

---
+++ bool.py
@@ -0,0 +1,40 @@
+a = True
+b = False
+print("a =", a)
+print("b =", b)
+
+
+# type(x)는 x의 자료형을 확인하는 파이썬의 내장 함수이다.
+print("type(a)", type(a))
+print("type(b)", type(b))
+
+
+print (" 1 == 1 : ", 1 == 1)
+
+
+# 자료형의 참(1)과 거짓(0)
+print("문자열, 리스트, 튜플, 딕셔너리 등 값이 비어있으면 거짓.")
+
+a = [1,2,3,4]
+print("while a:\n\tprint(a.pop())")
+while a:
+    print(a.pop())
+
+print("if[]")
+if[]:
+    print("True")
+else:
+    print("False")
+
+print("if[1,2,3]")
+if[1,2,3]:
+    print("True")
+else:
+    print("False")
+
+
+# 불(bool)
+print("bool('python') : ", bool('python'))
+print("bool('') : ", bool(''))
+
+
